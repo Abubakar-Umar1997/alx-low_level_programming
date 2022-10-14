a746a37caed00f2dddd03bee58a2590c1e9d2a50
Abubakar <abubakarumarshehu97@gnail.com>
2022-10-13 17:08:15 -0700

    commit 4&5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 /**
- * * main - Printing
- * *
- * * Return: Always 0 (Successful)
- *
+ * main - Printing
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual
-			puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
