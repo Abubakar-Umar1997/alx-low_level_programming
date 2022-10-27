5ed004a49fd782922c2d2021c16adb09fa00a2ae
Abubakar <abubakarumarshehu97@gnail.com>
2022-10-27 15:08:45 -0700

    strcpy 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,8 @@ char	*_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n && src[i])
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++
-	}
 	while (i < n)
 	{
 		dest[i] = '\0';
